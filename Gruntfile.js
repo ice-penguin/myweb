d431ac66f1add83f922965dc7e757baa8b9c602a
ice-penguin <hanjian1106@163.com>
2016-09-15 09:49:47 +0800

    init

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,7 +349,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'assets/images/**/*',
+            'assets/**/*',
             'index.html'
           ]
         }, {
