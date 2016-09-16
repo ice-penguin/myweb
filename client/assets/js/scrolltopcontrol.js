80d558a29f0f05d1476e9ec55071cf08c726a148
ice-penguin <hanjian1106@163.com>
2016-09-16 10:55:21 +0800

    js-init

--- client/assets/js/scrolltopcontrol.js
+++ client/assets/js/scrolltopcontrol.js
@@ -5,7 +5,7 @@ var scrolltotop={
 		scrollduration:400, //滚动过渡时间
 		fadeduration:[500,100] //淡出淡现消失
 	},
-	controlHTML:'<img src="templets/default/images/topback.gif"/>', //返回顶部按钮
+	controlHTML:'<img src="assets/images/topback.gif"/>', //返回顶部按钮
 	controlattrs:{offsetx:10,offsety:80},//返回按钮固定位置
 	anchorkeyword:"#top",
 	state:{
